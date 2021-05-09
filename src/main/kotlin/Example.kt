881144a939b412337e58664bfdfc944d85123568
SulthanDudekula <sulthan.cse27@gmail.com>
2021-05-09 13:06:08 +0530

    Second Commit

--- src/main/kotlin/Example.kt
+++ src/main/kotlin/Example.kt
@@ -7,8 +7,8 @@ fun main(args:Array<String>){
     val Name = "Ramaraju"
     val Age = 20
     val Branch = "CSE"
-    
-
+    val percentage = 75.5
+    println("Name : $Name  Age: $Age Branch: $Branch percentage: $percentage")
 }
 
 
